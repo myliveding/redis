f20732c18e6e666f42792ce80bb0b0c8ba6b6341
dingzr <dingzr@xbzoom.com>
2019-06-10 14:28:26 +0800

    redis单机模式，整合lettuct基本操作+键空间通知+redisson的分布式锁

--- PROJECT.md
+++ PROJECT.md
@@ -7,3 +7,30 @@ luttuce 主要用于rdis基本操作的场景以及键空间通知，因为redis
 
 luttuce 和 redisson都是线程安全的，原则上说可以不使用线程池，实际是本着防范于未然这个原则，
 我们还是要加上这个。
+
+
+## 疑问
+这个网站上面的问题跟我的差不多，但是一直没有回复：
+https://www.v2ex.com/t/542261
+
+下面是我运行之后的异常信息，但是不报错~
+2019-06-10 14:23:54.901 ERROR 13276 --- [sson-netty-6-24] o.redisson.client.handler.CommandsQueue  : Exception occured. Channel: [id: 0xaa31a78c, L:/127.0.0.1:55217 - R:127.0.0.1/127.0.0.1:6379]
+
+java.io.IOException: 远程主机强迫关闭了一个现有的连接。
+	at sun.nio.ch.SocketDispatcher.read0(Native Method)
+	at sun.nio.ch.SocketDispatcher.read(SocketDispatcher.java:43)
+	at sun.nio.ch.IOUtil.readIntoNativeBuffer(IOUtil.java:223)
+	at sun.nio.ch.IOUtil.read(IOUtil.java:192)
+	at sun.nio.ch.SocketChannelImpl.read(SocketChannelImpl.java:380)
+	at io.netty.buffer.PooledUnsafeDirectByteBuf.setBytes(PooledUnsafeDirectByteBuf.java:288)
+	at io.netty.buffer.AbstractByteBuf.writeBytes(AbstractByteBuf.java:1125)
+	at io.netty.channel.socket.nio.NioSocketChannel.doReadBytes(NioSocketChannel.java:347)
+	at io.netty.channel.nio.AbstractNioByteChannel$NioByteUnsafe.read(AbstractNioByteChannel.java:148)
+	at io.netty.channel.nio.NioEventLoop.processSelectedKey(NioEventLoop.java:682)
+	at io.netty.channel.nio.NioEventLoop.processSelectedKeysOptimized(NioEventLoop.java:617)
+	at io.netty.channel.nio.NioEventLoop.processSelectedKeys(NioEventLoop.java:534)
+	at io.netty.channel.nio.NioEventLoop.run(NioEventLoop.java:496)
+	at io.netty.util.concurrent.SingleThreadEventExecutor$5.run(SingleThreadEventExecutor.java:906)
+	at io.netty.util.internal.ThreadExecutorMap$2.run(ThreadExecutorMap.java:74)
+	at io.netty.util.concurrent.FastThreadLocalRunnable.run(FastThreadLocalRunnable.java:30)
+	at java.lang.Thread.run(Thread.java:745)
--- src/test/java/cn/dzr/study/RedisApplicationTests.java
+++ src/test/java/cn/dzr/study/RedisApplicationTests.java
@@ -1,5 +1,7 @@
 package cn.dzr.study;
 
+import cn.dzr.study.service.DistributedLocker;
+import cn.dzr.study.util.RedissLockUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,12 @@ public class RedisApplicationTests {
     @Autowired
     private RedisTemplate redisTemplateForListen;
 
+    @Autowired
+    private DistributedLocker distributedLocker;
+
+
     @Test
-    public void contextLoads() {
+    public void contextLoadsRedis() {
 
         redisTemplate.opsForValue().set("redis", "redis");
         redisTemplate.expire("redis", 1, TimeUnit.MINUTES);
@@ -30,6 +36,44 @@ public class RedisApplicationTests {
         redisTemplateForListen.opsForValue().set("redis", "redis");
         redisTemplateForListen.expire("redis", 1, TimeUnit.MINUTES);
         System.err.println(redisTemplateForListen.opsForValue().get("redis"));
+
+    }
+
+
+    @Test
+    public void contextLoadsServiceImpl() {
+        String lockKey = "testRedisson";//分布式锁的key
+        //执行的业务代码
+        try {
+            for (int i = 0; i < 10; i++) {
+                //设置60秒自动释放锁  （默认是30秒自动过期）
+                distributedLocker.lock(lockKey, TimeUnit.SECONDS, 60);
+                System.err.println("执行业务逻辑...");
+                Thread.sleep(10000);
+                distributedLocker.unlock(lockKey); //释放锁
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void contextLoadsUtil() {
+        String lockKey = "testRedisson-1";//分布式锁的key
+        try {
+            //执行的业务代码
+//        for (int i = 0; i < 10; i++) {
+            //设置60秒自动释放锁  （默认是30秒自动过期）
+            RedissLockUtil.lock(lockKey, TimeUnit.SECONDS, 60);
+            System.err.println("执行业务逻辑...");
+
+            Thread.sleep(3000);
+
+            RedissLockUtil.unlock(lockKey); //释放锁
+//        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
